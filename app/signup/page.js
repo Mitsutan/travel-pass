e557b6b32ba9c81c6bb9683350b8903d7fad2198
Mitsutan <petitcom_hondou@icloud.com>
2023-10-07 22:46:07 +0900

    firebase認証機能
    メールアドレスを用いてユーザー登録ができるようになった（ログインはまだ）

---
+++ app/signup/page.js
@@ -0,0 +1,65 @@
+"use client"
+import { FC, useEffect, useState } from 'react'
+// import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import { auth } from '../lib/firebase/firebase-conf';
+
+
+const SignUp = () => {
+  // console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+  // const router = useRouter()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      // user && router.push('/')
+    })
+  }, [])
+
+  const createUser = async (e) => {
+    e.preventDefault()
+    try {
+      await createUserWithEmailAndPassword(auth, email, password)
+      await sendEmailVerification(auth.currentUser)
+      // router.push('/sent')
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
+
+  return (
+    <div>
+      <form onSubmit={createUser}>
+        <div>
+          <label htmlFor="email">
+            Email:{' '}
+          </label>
+          <input
+            id="email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">
+            Password:{' '}
+          </label>
+          <input
+            id="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">
+          SignUp
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
